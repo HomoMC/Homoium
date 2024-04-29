2114952dbd28b9997eb60efc88c9db0ec34a590d
HaHaWTH <id_cn00@outlook.com>
2024-04-29 19:59:24 +0800

    1.0.8

--- src/main/java/io/wdsj/homoium/Homoium.java
+++ src/main/java/io/wdsj/homoium/Homoium.java
@@ -13,7 +13,7 @@ import static io.wdsj.homoium.HomoiumPlugin.isClient;
 public class Homoium {
     public static final String MOD_ID = "homoium";
     public static final String MOD_NAME = "Homoium";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
     public static final String DEPENDENCY = "required-after:mixinbooter@[8.8,);required-after:configanytime;";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
